d223c7ef81089879e046c64c9887dd3495f2296d
Harry Lzh <109070644+YGMssMGY@users.noreply.github.com>
2024-03-31 07:11:23 +0000

    codes

--- c++/Broken Necklace.cpp
+++ c++/Broken Necklace.cpp
@@ -1,1 +1,39 @@
 #include<bits/stdc++.h>
+using namespace std;
+
+const int NR = 400;
+int n, ans;
+char s[NR];
+int main()
+{
+    cin>>n>>s;
+    for(int i = 0; i <n; i++){
+        int blue = 0;
+        for(int j = 0; j< n; j++){
+            if(s[(i-j+n)%n] == 'r'){
+                break;
+            }
+            else blue++;
+        }
+        int red = 0; 
+        for(int j = 0; j < n; j++){
+            if(s[(i-j+n)%n] == 'b') break;
+            else red++;
+        }
+        int leftCnt = max(blue, red);
+        blue = 0;
+        for(int j = 1; j <= n; j++){
+            if(s[(i+j)%n] == 'r') break;
+            else blue++;
+        }
+        red = 0;
+        for(int j = 1; j <= n; j++){
+            if(s[(i+j)%n] == 'b') break;
+            else red++;
+        }
+        int rightCnt = max(blue, red);
+        ans = max(ans, leftCnt + rightCnt);
+    }
+    cout<<min(ans, n)<<endl;
+    return 0;
+}
---
+++ c++/milking cow.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<pair<int, int>> times(N);
+    for (int i = 0; i < N; i++) {
+        cin >> times[i].first >> times[i].second;
+    }
+    sort(times.begin(), times.end());
+
+    int longest_milk = 0, longest_idle = 0;
+    int start = times[0].first, end = times[0].second;
+    for (int i = 1; i < N; i++) {
+        if (times[i].first <= end) {
+            end = max(end, times[i].second);
+        } else {
+            longest_milk = max(longest_milk, end - start);
+            longest_idle = max(longest_idle, times[i].first - end);
+            start = times[i].first;
+            end = times[i].second;
+        }
+    }
+    longest_milk = max(longest_milk, end - start);
+
+    cout << longest_milk << " " << longest_idle << endl;
+
+    return 0;
+}
