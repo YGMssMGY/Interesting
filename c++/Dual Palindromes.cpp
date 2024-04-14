5ed4294c9aefdad87956ae7319351bc8d443a343
Harry Lzh <109070644+YGMssMGY@users.noreply.github.com>
2024-04-14 05:05:13 +0000

    dual P usaco

---
+++ c++/Dual Palindromes.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string toBaseB(int num, int B) {
+    string res = "";
+    while (num > 0) {
+        int digit = num % B;
+        if (digit < 10) res += digit + '0';
+        else res += digit - 10 + 'A';
+        num /= B;
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+bool isPalindrome(string str) {
+    string rev = str;
+    reverse(rev.begin(), rev.end());
+    return str == rev;
+}
+
+int main() {
+    int N, S;
+    cin >> N >> S;
+    for (int num = S + 1, count = 0; count < N; num++) {
+        int baseCount = 0;
+        for (int B = 2; B <= 10; B++) {
+            if (isPalindrome(toBaseB(num, B))) {
+                baseCount++;
+            }
+            if (baseCount >= 2) {
+                cout << num << endl;
+                count++;
+                break;
+            }
+        }
+    }
+    return 0;
+}
