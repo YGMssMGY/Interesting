e6788725114af99084f14da12e0dbecf3db09c4c
Harry Lzh <109070644+YGMssMGY@users.noreply.github.com>
2024-04-21 07:32:12 +0000

    mm

--- c++/MotherMilk.cpp
+++ c++/MotherMilk.cpp
@@ -10,16 +10,16 @@ void dfs(int a, int b, int c) {
     vis[a][b][c] = true;
     if (a == 0) ans.insert(c);
     if (a > 0) {
-        if (b < B) dfs(max(0, a - (B - b)), min(B, b + a), c); // pour from A to B
-        if (c < C) dfs(max(0, a - (C - c)), b, min(C, c + a)); // pour from A to C
+        if (b < B) dfs(max(0, a - (B - b)), min(B, b + a), c); 
+        if (c < C) dfs(max(0, a - (C - c)), b, min(C, c + a)); 
     }
     if (b > 0) {
-        if (a < A) dfs(min(A, a + b), max(0, b - (A - a)), c); // pour from B to A
-        if (c < C) dfs(a, max(0, b - (C - c)), min(C, c + b)); // pour from B to C
+        if (a < A) dfs(min(A, a + b), max(0, b - (A - a)), c); 
+        if (c < C) dfs(a, max(0, b - (C - c)), min(C, c + b)); 
     }
     if (c > 0) {
-        if (a < A) dfs(min(A, a + c), b, max(0, c - (A - a))); // pour from C to A
-        if (b < B) dfs(a, min(B, b + c), max(0, c - (B - b))); // pour from C to B
+        if (a < A) dfs(min(A, a + c), b, max(0, c - (A - a))); 
+        if (b < B) dfs(a, min(B, b + c), max(0, c - (B - b))); 
     }
 }
 
@@ -27,6 +27,6 @@ int main() {
     cin >> A >> B >> C;
     dfs(0, 0, C);
     for (int c : ans) cout << c << " ";
-    cout << endl;
+    cout<<endl;
     return 0;
 }
